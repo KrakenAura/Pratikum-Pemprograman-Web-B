b2154d95e7feab69065e00acd73665731e6445a7
KrakenAura <akarim18641@gmail.com>
2023-12-17 11:55:58 +0700

    Add Demo Modul 6

--- Modul 6/demo-modul6/routes/api.php
+++ Modul 6/demo-modul6/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/games', [GameController::class, 'index']);
+Route::get('/games/{id}', [GameController::class, 'show']);
+Route::post('/games', [GameController::class, 'store']);
+Route::put('/games/{id}', [GameController::class, 'update']);
+Route::delete('/games/{id}', [GameController::class, 'destroy']);
