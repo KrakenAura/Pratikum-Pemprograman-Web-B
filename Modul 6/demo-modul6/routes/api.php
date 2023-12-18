c38deb22702ac47298cb24b70d1946803e00848d
KrakenAura <akarim18641@gmail.com>
2023-12-18 14:44:57 +0700

    Add Generate Access Token

--- Modul 6/demo-modul6/routes/api.php
+++ Modul 6/demo-modul6/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,4 @@ Route::get('/games/{id}', [GameController::class, 'show']);
 Route::post('/games', [GameController::class, 'store']);
 Route::put('/games/{id}', [GameController::class, 'update']);
 Route::delete('/games/{id}', [GameController::class, 'destroy']);
+Route::post('/login', [AuthController::class, 'login']);
